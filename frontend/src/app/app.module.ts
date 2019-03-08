68ab4732f3349dcda030da587918ec7b5ae45ec4
Antoine S <antoine.stempfer@epitech.eu>
2019-03-08 22:41:08 +0100

    Added base screens

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {MainMenuComponent} from './main-menu/main-menu.component';
+import {LoginComponent} from './login/login.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
-import { FashionWeekComponentComponent } from './fashion-week-component/fashion-week-component.component';
+import {SpectateComponent} from './spectate/spectate.component';
+import {UserComponent} from './user/user.component';
+import { OrderByScorePipe } from './order-by-score.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
-    FashionWeekComponentComponent
+    LoginComponent,
+    SpectateComponent,
+    UserComponent,
+    OrderByScorePipe
   ],
   imports: [
     BrowserModule,
@@ -18,6 +23,14 @@ import { FashionWeekComponentComponent } from './fashion-week-component/fashion-
       {
         path: '',
         component: MainMenuComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'spectate',
+        component: SpectateComponent
       }
     ])
   ],
